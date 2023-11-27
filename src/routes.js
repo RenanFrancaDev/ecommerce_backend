1c51a97dc962357f70598136291d7c9590f59130
Renan Franca <renanfranca.dev@gmail.com>
2023-11-27 20:53:19 -0300

    feat: routes predefined

---
+++ src/routes.js
@@ -0,0 +1,11 @@
+function routes(app) {
+    app.use('/users', require('../routes/users.js'));
+    app.use('/products', require('../routes/products.js'));
+    app.use('/orders', require('../routes/orders.js'));
+    app.use('/orders-items', require('../routes/order-items.js'));
+    app.use('/sales', require('../routes/sales.js'));
+    app.use('/stock', require('../routes/stock.js'));
+    return;
+  }
+  
+  module.exports = routes;
