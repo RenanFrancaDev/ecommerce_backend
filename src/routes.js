9bca3d27f1bc9a1330961da0473baaa9b78b4398
Renan Franca <renanfranca.dev@gmail.com>
2023-11-30 15:51:55 -0300

    feat: routes modificated

--- src/routes.js
+++ src/routes.js
@@ -2,8 +2,7 @@ function routes(app) {
     app.use('/users', require('../routes/users'));
     app.use('/products', require('../routes/products.js'));
     app.use('/stock', require('../routes/stock.js'));
-    // app.use('/order_product', require('../routes/order-items.js'));
-    // app.use('/sales', require('../routes/sales.js'));
+    app.use('/sales', require('../routes/sales.js'));
     return;
   }
   
